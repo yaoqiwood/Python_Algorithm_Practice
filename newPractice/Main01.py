3d715710c3fa9810f52d4203fa3f613ffc318d1a
yaoqiwood_office <yaoqiwood@gmail.com>
2022-06-01 17:59:35 +0800

    * day01

--- newPractice/Main01.py
+++ newPractice/Main01.py
@@ -6,8 +6,19 @@ class Solution:
         if (x1 != x2) and (target == v1 + v2):
           return [x1, x2]
 
+  def anotherTwoSum(self, nums, target):
+    # traverse the array
+    seen = {}
+    for i, value in enumerate(nums):  # 1
+      remaining = target - nums[i]  # 2
+
+      if remaining in seen:  # 3
+        return [i, seen[remaining]]  # 4
+      else:
+        seen[value] = i  # 5
+
 
 list = [3, 2, 4]
 
 solution = Solution()
-solution.twoSum(list, 6)
+print(solution.anotherTwoSum(list, 6))
