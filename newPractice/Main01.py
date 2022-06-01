61928c5314e1d7e0d901db8891c7958b0878c679
yaoqiwood_office <yaoqiwood@gmail.com>
2022-06-01 17:17:22 +0800

    *Leetcode day01

--- newPractice/Main01.py
+++ newPractice/Main01.py
@@ -1,2 +1,13 @@
+class Solution:
+  def twoSum(self, nums, target):
+    # traverse the array
+    for x1, v1 in enumerate(nums):
+      for x2, v2 in enumerate(nums):
+        if (x1 != x2) and (target == v1 + v2):
+          return [x1, x2]
 
-print("hello world")
+
+list = [3, 2, 4]
+
+solution = Solution()
+solution.twoSum(list, 6)
